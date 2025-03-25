87a7ba2ad75d319d5f22e19ac7a68f9b0eee8992
Ankur Gupta <ankurdotcom@gmail.com>
2025-03-25 12:00:48 +0530

    Initial code setup

---
+++ src/main/java/com/ankur/buggy_micro_service/BuggyMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ankur.buggy_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuggyMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuggyMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankur/buggy_micro_service/BuggyMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankur.buggy_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuggyMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
